115e26c453ee5ba0e48514db6682c2aedb447ac2
FizaAamir <144325138+FizaAamir@users.noreply.github.com>
2023-10-09 21:02:06 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+class Bulb:
+    status = False
+
+    def isOn(self):
+        self.status = True
+
+    def isOff(self):
+        self.status = False
+
+    def getStatus(self):
+        if self.status==True:
+            return "Bulb is Glowing"
+        elif self.status == False:
+            return "Bulb is Glowing"
+        else:
+            pass
+    
+    
+    
+
+
---
+++ test.py
@@ -0,0 +1,14 @@
+from main import Bulb
+
+bulb=Bulb()
+def test_bulb_isOff():
+    assert bulb.getstatus()=="Bulb id not glowing"
+    
+
+def test_bulb_isOn():
+    bulb.ison()
+    assert bulb.getstatus()=="Bulb is glowing"
+    
+#def test_getStatus():
+#    bulb.isOff()
+#    assert bulb.get
